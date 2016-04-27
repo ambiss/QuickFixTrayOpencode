b8715b218fcba4284f3d57068600bd2e90029211
Anderson Sonteam <ambiss@me.com>
2016-04-27 18:42:48 -0300

    Desligando Plugins
    #Script #JS Desligando os plugins de carousel

--- resources/devices/detect_device.js
+++ resources/devices/detect_device.js
@@ -28,10 +28,15 @@ var isDevice = {
 if(isDevice.tablet()) {
     jQuery('body').addClass('_istablet'); // insere classe tablet no elemento body para fins de estilo especifico
     jQuery('#cmdAllDep').removeClass('block-alldep').addClass('tablet-block-alldep'); // desliga o :hover dos tablets do alldep e permite o uso do hasclick
-    jQuery('.list-category section .subnivel').hide(); // desliga o :hover dos tablets
+    jQuery('.list-category section .subnivel').hide(); // desliga o :hover dos tablets no menu de categoria
     
     jQuery(".wrapper-category .alldep" ).click(function() {
       jQuery(this).toggleClass("has-click"); // habilita o click apos cancelamento do :hover [bloco acima]
     });
 }
 
+if(isDevice.device()) {
+    jQuery('body').addClass('_isdevice');
+    jQuery('.related-products ul').removeClass('showcase-carousel').addClass('slider-for-devices'); // necessario para desligar o slick e inserir slider for devies
+    jQuery('.produto-imagem-miniaturas .jcarousel-skin-tango').removeClass('jcarousel-skin-tango').addClass('slider-for-devices'); // necessario para desligar o jcarousel e inserir slider for devies
+}
