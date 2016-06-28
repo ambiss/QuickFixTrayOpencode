b4dbc1104e2d6e8e032cb7d0e3a32fc7198a2167
Anderson Sonteam <ambiss@me.com>
2016-06-28 14:56:38 -0300

    Box flutuante com temporizador
    Box flutuante pode ser exibida com temporizador e / ou datas, baseadas em segundos.
    
    ex: em producao -> Matecki

---
+++ float-window/float_box_date_clear.js
@@ -0,0 +1,33 @@
+jQuery(document).ready(function(){
+    
+    var currentDate = parseInt(new Date().getTime()/1000);
+
+    jQuery(".close-window").click( function() {
+        jQuery(".box-float").hide();
+        jQuery(".effects-shadow").hide();
+        localStorage.box_float = "show";
+        
+        // referente a 30 dias baseado em segundos
+        var additionalTime = 60*60*24*30;
+        
+        // save the future date
+        localStorage.setItem('modal_time', currentDate + additionalTime);
+    });
+    
+    jQuery(".effects-shadow").click( function() {
+        jQuery(".effects-shadow").hide();
+        jQuery(".box-float").hide();
+    });
+    
+    var dateSave = localStorage.getItem('modal_time');
+    if(currentDate > dateSave){
+        localStorage.removeItem('modal_time');
+        localStorage.removeItem('box_float');
+    }
+    
+    if (localStorage.box_float !== 'show') {
+        jQuery(".box-float").show();
+        jQuery(".effects-shadow").show();
+    }
+    
+});
